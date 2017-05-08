ce0267d43a3a03a17512101eff71c18a14c1b4d2
gcc <pollodaniel91@gmail.com>
2017-05-08 00:59:17 -0500

    cambios
    estilo

--- spreeDmendoza/app/overrides/inicio.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/shared/_main_nav_bar",
-	                 :replace_contents => "#home-link",
-	                 :text          => "<a href='/'>Inicio</a>",
-	                 :name          => "change_link")
